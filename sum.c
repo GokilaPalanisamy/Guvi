925f3ea4d0ae6e5f68842c9546e7992c8df7acfb
Gokila P <sarangoki005@gmail.com>
2017-04-21 11:52:40 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,sum;
+    printf("Enter the number do you want:");
+    scanf("%d",&a);
+    b=a+1;
+    sum=a*b/2;
+    printf("sum is %d",sum);
+    
+return 0;
+}
