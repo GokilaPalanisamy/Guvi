1a43425777b6cd90c03c95f2d2678d7133695712
Gokila P <sarangoki005@gmail.com>
2017-04-21 12:01:04 +0530

    Create forloop.c

---
+++ forloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   
+    int a,sum=0;
+    scanf("%d",&a);
+    for(int i=0;i<=a;i++)
+    {
+        sum=sum+i;
+       
+    }
+     printf("%d\n",sum);
+    
+return 0;
+}
