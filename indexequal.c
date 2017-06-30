4875f8b7e7b5aa2ffa3a3d8c99c593614ce7d2fc
Gokila P <sarangoki005@gmail.com>
2017-06-30 14:03:09 +0530

    Create indexequal.c

---
+++ indexequal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int Linearsearch(int a[], int n){
+int i;
+if(a[i]==i){
+return i;
+}
+return -1;
+int main(){
+int *a;
+scanf("%d",&n);
+a=(int)malloc(sizeof(int)*n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("Value is equal %d",Linearsearch(a,n));
+getchar();
+return 0;
+}
