159874448052f26ebab939c620c0c28a230672ed
Gokila P <sarangoki005@gmail.com>
2017-07-01 10:35:52 +0530

    Update indexequal.c

--- indexequal.c
+++ indexequal.c
@@ -5,10 +5,11 @@ if(a[i]==i){
 return i;
 }
 return -1;
+}
 int main(){
 int *a;
 scanf("%d",&n);
-a=(int)malloc(sizeof(int)*n);
+a=(int*)malloc(sizeof(int)*n);
 for(i=0;i<n;i++)
 scanf("%d",&a[i]);
 printf("Value is equal %d",Linearsearch(a,n));
